a5cadf3565123198be38b9bdaf38cf343c7ec2bd
Kunal3723 <78958213+Kunal3723@users.noreply.github.com>
2022-02-12 15:27:34 +0530

    make it better

--- index.js
+++ index.js
@@ -18,11 +18,8 @@ app.get('/',function(req,res){
   res.send("Hello to Memories API");
 });
 
-
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-
-//mongoose.set('useFindAndModify', false);
